69c90d29b75c5e00255be1a79c9e3da96163c02d
fahmy-dev <faahmy@proton.me>
2024-12-01 15:43:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name, word) {
+    const cards = [];
+    for (let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${word} gift!`)
+    }
+    return cards;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    let i = number;
+    while (number >= 0) {
+        console.log(number);
+        number--
+        //debugger;
+    }
+    return number;
+}
+
+console.log(countDown(10));
